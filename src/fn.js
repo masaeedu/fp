823335c07d296d3c0b9f91bb5fe57edbb05cd957
Asad Saeeduddin <masaeedu@gmail.com>
2018-03-28 22:30:31 -0400

    Add konst

--- src/fn.js
+++ src/fn.js
@@ -1,6 +1,7 @@
 // Misc
 export const pipe = fs => fs.reduce((g, f) => a => f(g(a)));
 export const id = _ => _;
+export const konst = x => _ => x;
 
 // Identity
 export const is = x => typeof x === "function";
