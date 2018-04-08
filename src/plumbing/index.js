aec632f397a089e34ebef7a9a2a529cb794eb6bf
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-08 16:23:20 -0400

    Refactor plumbing/index.js

--- src/plumbing/index.js
+++ src/plumbing/index.js
@@ -1,28 +1,21 @@
-// NB: Not allowed to reference *decorated* typeclasses directly from ./index.js
-//     because this is circular
+// NB: Not allowed to reference augmented typeclasses directly from ./index.js
+//     because this would be circular 😞
 import * as Obj from "../obj";
 import * as Iter from "../iter";
 
-// TODO: Maybe delete
-// ------------------------
-// NB: Not allowed to reference any of our nice FP code in here unfortunately 😞
-// Gets too confusing reasoning about circular dependencies
-// ****Or does it?
-// ------------------------
+const augmentDef = def => ({ impl, deps }) => {
+  const hasImpl = Obj.keys(impl).some(k => !!def[k]);
+  const hasDeps = deps.every(k => !!def[k]);
+
+  return hasImpl || !hasDeps
+    ? def
+    : impl |> Obj.map(i => i(def)) |> Obj.append(def);
+};
 
 // Typeclass wrapper
 export const implement = ({ mdefs, methods }) => candidate => {
   // Add every available default definition that isn't explicitly specified
-  const fullDef =
-    mdefs
-    |> Iter.fold(def => ({ impl, deps }) => {
-      const hasImpl = Obj.keys(impl).some(k => !!def[k]);
-      const hasDeps = deps.every(k => !!def[k]);
-
-      return hasImpl || !hasDeps
-        ? def
-        : impl |> Obj.map(i => i(def)) |> Obj.append(def);
-    })(candidate);
+  const fullDef = mdefs |> Iter.fold(augmentDef)(candidate);
 
   // Add the methods
   const withMethods = { ...fullDef, ...(fullDef |> methods) };
