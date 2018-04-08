c7c5987e83499f88c7b45207010024d50ed28b16
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-08 15:22:21 -0400

    Add Fn.flip

--- src/fn.js
+++ src/fn.js
@@ -4,6 +4,7 @@ export const pipe = fs => fs.reduce((g, f) => a => f(g(a)));
 export const id = _ => _;
 const _const = x => _ => x;
 export { _const as const };
+export const flip = f => x => y => f(y)(x);
 
 // Identity
 export const is = x => typeof x === "function";
