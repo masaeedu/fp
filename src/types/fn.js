2ea289cd7ec9ceefcdb20ca26be1aa178cf90945
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-13 04:13:28 -0400

    Add base case for pipe

--- src/types/fn.js
+++ src/types/fn.js
@@ -1,10 +1,12 @@
+import * as Arr from "./arr";
+
 // Misc
 export const compose = f => g => a => f(g(a));
-export const pipe = fs => fs.reduce((g, f) => a => f(g(a)));
 export const id = _ => _;
 const _const = x => _ => x;
 export { _const as const };
 export const flip = f => x => y => f(y)(x);
+export const pipe = Arr.foldl(flip(compose))(id); // TODO: Consider manually inlining for performance
 
 // Identity
 export const is = x => typeof x === "function";
