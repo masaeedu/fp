c35912080e68239e687333279dbca22f7a2882f4
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-15 03:58:44 -0400

    Refactor to use functor composition

--- src/index.js
+++ src/index.js
@@ -3,24 +3,14 @@ import { implement } from "./plumbing";
 import * as Types from "./types";
 import * as Classes from "./classes";
 
-const { Obj, Arr, Fn } = Types;
+const { Obj, Arr, Fn, Functor } = Types;
 
-// Nesting functors and applicatives
-const nestF = ({ map: map1 }) => ({ map: map2 }) => ({
-  map: Fn.compose(map1)(map2)
-});
-
-const nestA = ({ of: o1, lift2: l1 }) => ({ of: o2, lift2: l2 }) => ({
-  of: Fn.compose(o1)(o2),
-  lift2: Fn.compose(l1)(l2)
-});
-
-const ObjOfArr = { ...nestF(Obj)(Arr), ...nestA(Obj)(Arr) };
+const ObjOfArr = Functor.compose(Obj)(Arr);
 
 const implementations = {
   Arr: ["Foldable", "Functor", "Apply", "Chain", "Traversable"],
   Either: ["Functor", "Apply", "Chain"],
-  Iter: ["Foldable", "Functor", "Apply", "Chain"],
+  Iter: ["Functor", "Apply", "Chain", "Traversable", "Foldable"],
   Obj: ["Foldable", "Functor", "Traversable"]
 };
 
