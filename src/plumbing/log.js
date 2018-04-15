7dff07c1e6122f75b88ac24597831c769da7a577
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-15 03:49:22 -0400

    Add ability to log with message

--- src/plumbing/log.js
+++ src/plumbing/log.js
@@ -1,7 +1,9 @@
-export const log = value => {
+export const logMsg = msg => value => {
   const { stack } = new Error();
   const caller = stack.split("\n").slice(2);
 
-  console.log({ value, caller });
+  console.log(msg !== undefined ? { value, caller, msg } : { value, caller });
   return value;
 };
+
+export const log = logMsg(undefined);
