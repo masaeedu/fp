a6c1059df8447c7b64d8caffbd322621078d2e28
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-15 21:57:37 -0400

    Fix argument ordering bug

--- src/types/prm.js
+++ src/types/prm.js
@@ -5,7 +5,8 @@ export const map = f => p => p.then(f);
 
 // Applicative
 export const of = x => Promise.resolve(x);
-export const lift2 = f => p1 => chain(x => p1 |> map(f(x)));
+export const lift2 = f => p1 => p2 =>
+  p2 |> chain(y => p1 |> chain(x => f(x)(y)));
 
 // Monad
 export const chain = map; // http://bit.ly/2FQIBGV
