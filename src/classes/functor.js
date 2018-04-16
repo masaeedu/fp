480093792ce6b33d054d08bbac003d374dfbc1e0
Asad Saeeduddin <masaeedu@gmail.com>
2018-04-15 21:57:04 -0400

    Add derivation of map from traverse

--- src/classes/functor.js
+++ src/classes/functor.js
@@ -1,8 +1,14 @@
+import { Identity } from "../types/fnctr";
+
 // Equivalent minimal definitions
 export const mdefs = (() => {
   const map = ({ of, chain }) => f => chain(v => f(v) |> of);
+  const mapFromTraverse = ({ traverse }) => traverse(Identity);
 
-  return [{ impl: { map }, deps: ["of", "chain"] }];
+  return [
+    { impl: { map }, deps: ["of", "chain"] },
+    { impl: { map: mapFromTraverse }, deps: ["traverse"] }
+  ];
 })();
 
 // Class methods
