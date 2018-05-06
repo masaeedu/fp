b05ed6c3f9ae1263df35824266046508969ccadd
Asad Saeeduddin <masaeedu@gmail.com>
2018-05-06 03:46:18 -0400

    Extend ADT tests

--- src/adt/index.test.js
+++ src/adt/index.test.js
@@ -1,14 +1,14 @@
 import { test } from "ava";
-import { adt, a } from ".";
+import { adt, _ } from ".";
 
 test("adt works", t => {
-  const { F, match } = adt({ F: [a, a] });
+  const { F, match } = adt({ F: [_, _] });
 
   t.snapshot(F(5)(10) |> match({ F: a => b => a + b }));
 });
 
 test("either adt", t => {
-  const { L, R, match } = adt({ L: [a], R: [a] });
+  const { L, R, match } = adt({ L: [_], R: [_] });
 
   t.snapshot(L(5) |> match({ L, R: x => x + 1 }));
   t.snapshot(R(5) |> match({ L, R: x => x + 1 }));
@@ -16,7 +16,7 @@ test("either adt", t => {
 
 test("nested types", t => {
   const Bool = adt({ T: [], F: [] });
-  const Vld = adt({ V: [a, a] }); // Bool, String
+  const Vld = adt({ V: [_, _] }); // Bool, String
 
   const { T, F } = Bool;
   const { V } = Vld;
@@ -28,4 +28,13 @@ test("nested types", t => {
       |> Vld.match({ V: Bool.match({ F: V(F), T: x => x + 1 }) })
       |> t.snapshot;
   }
+});
+
+test("zero argument type", t => {
+  const { Nothing, Just, match } = adt({ Nothing: [], Just: [_] });
+
+  t.snapshot(Just(10));
+  t.snapshot(Nothing);
+  t.snapshot(Nothing |> match({ Nothing: 0, Just: x => x * 2 }));
+  t.snapshot(Just(10) |> match({ Nothing: 0, Just: x => x * 2 }));
 });
