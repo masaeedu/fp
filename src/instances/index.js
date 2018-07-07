54ff8ee54ab33f4cc916e4c83fffe04fee5b3fce
Asad Saeeduddin <masaeedu@gmail.com>
2018-07-07 02:53:59 -0400

    Fix up all imports and re-exports for instances

--- src/instances/index.js
+++ src/instances/index.js
@@ -1,16 +1,25 @@
-// Types
-import * as Fn from "./fn";
-import * as Prm from "./prm";
-import * as State from "./state";
-import * as Unit from "./unit";
-import * as Str from "./str";
-import * as Fnctr from "./fnctr";
-import * as Meta from "./meta";
-
-export { Fn, Prm, State, Unit, Str, Fnctr, Meta };
-
-export * from "./int/augmented";
 export * from "./arr/augmented";
+import * as Cont from "./cont";
+export { Cont };
+export * from "./genericeithert";
 export * from "./either/augmented";
+export * from "./fn/augmented";
+import * as Fnctr from "./fnctr";
+export { Fnctr };
+import GenericList from "./genericlist";
+export { GenericList };
+export * from "./int/augmented";
 export * from "./iter/augmented";
+import * as Maybe from "./maybe";
+export { Maybe };
 export * from "./obj/augmented";
+import * as Prm from "./prm";
+export { Prm };
+import ReaderT from "./readert";
+export { ReaderT };
+import * as State from "./state";
+export { State };
+import * as Str from "./str";
+export { Str };
+import * as Unit from "./unit";
+export { Unit };
