4aa36b93538dc18475d004e167ad0486928166f8
Asad Saeeduddin <masaeedu@gmail.com>
2018-07-07 02:46:34 -0400

    Move misc stuff under src/instances/meta

--- src/instances/misc.js
+++ src/instances/misc.js
@@ -1,6 +1,3 @@
-import * as _Transformers from "./transformers";
-export const Transformers = _Transformers;
-
 export const MonoidalApplicative = M => A => {
   const empty = A.pure(M.empty);
   const append = A.lift2(M.append);
