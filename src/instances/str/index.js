bea6eb1926b4779c35fa01d9358cf9c6e2c921db
Asad Saeeduddin <masaeedu@gmail.com>
2018-08-05 15:38:51 -0400

    Add constructor algebra to Str

--- src/instances/str/index.js
+++ src/instances/str/index.js
@@ -1,6 +1,12 @@
 // Identity
 export const is = x => typeof x === "string";
 
+// Algebra
+export const Nil = "";
+export const Cons = x => xs => `${x}${xs}`;
+export const match = ({ Nil, Cons }) => s =>
+  s === "" ? Nil : Cons(s[0])(s.slice(1));
+
 // Monoid
 export const empty = "";
 export const append = a => b => a + b;
