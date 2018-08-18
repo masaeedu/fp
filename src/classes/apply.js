a711c71b07fc9013007d52d652ad0bf0ba08e6e3
Asad Saeeduddin <masaeedu@gmail.com>
2018-08-17 21:00:51 -0400

    Rename Apply mdefs

--- src/classes/apply.js
+++ src/classes/apply.js
@@ -2,15 +2,16 @@ import * as Fn from "../instances/fn";
 
 // Equivalent minimal definitions
 export const mdefs = (() => {
-  const ap = ({ lift2 }) => lift2(Fn.id);
-  const lift2 = ({ map, ap }) => f => x1 => x2 => ap(map(f)(x1))(x2);
+  const apFromLift2 = ({ lift2 }) => lift2(Fn.id);
+  const lift2FromMapAndAp = ({ map, ap }) => f => x1 => x2 =>
+    ap(map(f)(x1))(x2);
   const apFromOfAndChain = ({ of, chain }) => af => av =>
     af |> chain(f => av |> chain(v => f(v) |> of));
 
   return [
     { impl: { ap: apFromOfAndChain }, deps: ["of", "chain"] },
-    { impl: { ap }, deps: ["lift2"] },
-    { impl: { lift2 }, deps: ["map", "ap"] }
+    { impl: { ap: apFromLift2 }, deps: ["lift2"] },
+    { impl: { lift2: lift2FromMapAndAp }, deps: ["map", "ap"] }
   ];
 })();
 
@@ -18,6 +19,7 @@ export const mdefs = (() => {
 export const methods = ({ map, of, ap, lift2 }) => {
   const pa = Fn.flip(ap);
   const pam = Fn.flip(map);
+  const apFirst = Fn.const;
 
-  return { pa, pam };
+  return { pa, pam, apFirst };
 };
