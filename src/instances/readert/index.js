c498e826ab77850fc9d61612da972ee6389303e6
Asad Saeeduddin <masaeedu@gmail.com>
2018-11-16 01:12:27 -0500

    Fix, refactor ReaderT instance
    Has the same issue with derivations being order dependent. Really need
    to implement derivation solving.
    
    Also rearranged some comments and annotations and stuff

--- src/instances/readert/index.js
+++ src/instances/readert/index.js
@@ -2,15 +2,22 @@ import { Fn } from "..";
 import { implement } from "../../plumbing";
 import { Functor, Apply, Chain } from "../../classes";
 
+// :: type ReaderT r m a = r -> m a
+
+// :: Monad m -> Monad (ReaderT r m)
 const ReaderT = M => {
-  // Monad
-  // :: x -> (r -> m x)
+  // :: x -> ReaderT r m x
   const of = x => Fn.const(M.of(x));
-  // :: (a -> r -> m b) -> (r -> m a) -> (r -> m b)
-  const chain = f => x => r => x(r) |> M.chain(a => f(a)(r));
+
+  // :: (a -> ReaderT r m b) -> ReaderT r m a -> ReaderT r m b
+  const chain = armb => rma => r => rma(r) |> M.chain(a => armb(a)(r));
 
   return (
-    { of, chain } |> implement(Chain) |> implement(Apply) |> implement(Functor)
+    { of, chain }
+    |> implement(Chain)
+    |> implement(Apply)
+    |> implement(Functor)
+    |> implement(Apply)
   );
 };
 
