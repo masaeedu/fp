d98a3af573a55d2f85bf657ba953d5c740eefc11
Asad Saeeduddin <masaeedu@gmail.com>
2018-12-20 23:45:36 -0500

    Refactor Fn to stop using import/export

--- src/instances/fn/index.js
+++ src/instances/fn/index.js
@@ -1,31 +1,49 @@
-import * as Arr from "../arr";
+const Arr = require("../arr");
 
-// Category
-export const id = x => x;
-export const compose = f => g => a => f(g(a));
+const Fn = (() => {
+  const _ = {};
 
-// Misc
-const _const = x => _ => x;
-export { _const as const };
-export const flip = f => x => y => f(y)(x);
-export const pipe = fs => x => fs.reduce((p, f) => f(p), x);
-export const passthru = flip(pipe);
-export const uncurry = Arr.foldl(id);
-export const curryN = n => f => {
-  const loop = a => acc => (a === 0 ? f(acc) : x => loop(a - 1)([...acc, x]));
-  return loop(n)([]);
-};
-export const feed = x => f => f(x);
+  // Identifiable
+  const is = x => typeof x === "function";
 
-// Identity
-export const is = x => typeof x === "function";
+  // Category
+  const id = x => x;
+  const compose = f => g => a => f(g(a));
 
-// Functor
-export const map = compose;
+  // Misc
+  _["const"] = x => _ => x;
+  const flip = f => x => y => f(y)(x);
+  const pipe = fs => x => fs.reduce((p, f) => f(p), x);
+  const passthru = flip(pipe);
+  const uncurry = Arr.foldl(id);
+  const curryN = n => f => {
+    const loop = a => acc => (a === 0 ? f(acc) : x => loop(a - 1)([...acc, x]));
+    return loop(n)([]);
+  };
+  const feed = x => f => f(x);
+  _["$"] = _["|>"] = id;
 
-// Applicative
-export const of = _const;
-export const lift2 = f => fn1 => fn2 => x => f(fn1(x))(fn2(x));
+  // Functor
+  const map = compose;
 
-// Chain
-export const chain = f => fn => x => f(fn(x))(x);
+  // Chain
+  const of = _["const"];
+  const chain = f => fn => x => f(fn(x))(x);
+
+  // prettier-ignore
+  return {
+    ..._,
+    // Identifiable
+    is,
+    // Category
+    id, compose,
+    // Misc
+    flip, pipe, passthru, uncurry, curryN, feed,
+    // Functor
+    map,
+    // Chain
+    of, chain
+  };
+})();
+
+module.exports = Fn;
