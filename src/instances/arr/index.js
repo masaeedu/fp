3c8ba376291ee30c12052f4ec5a97046937ae198
Asad Saeeduddin <masaeedu@gmail.com>
2018-12-20 20:21:00 -0500

    Refactor Arr to stop using import/export

--- src/instances/arr/index.js
+++ src/instances/arr/index.js
@@ -1,37 +1,60 @@
-// Misc
-export const last = as => as[as.length - 1];
-export const lastN = n => as => as.slice(as.length - n);
-export const range = n => [...Array(n).keys()];
-export const filter = f => xs => xs.filter(f);
-export const replicate = n => v => range(n).map(_ => v);
+const Arr = (() => {
+  // Misc
+  const last = as => as[as.length - 1];
+  const lastN = n => as => as.slice(as.length - n);
+  const range = n => [...Array(n).keys()];
+  const filter = f => xs => xs.filter(f);
+  const replicate = n => v => range(n).map(_ => v);
 
-// Algebra
-export const Nil = [];
-export const Cons = x => xs => [x, ...xs];
-export const match = ({ Nil, Cons }) => xs =>
-  xs.length === 0 ? Nil : Cons(xs[0])(xs.slice(1));
+  // Constructors
+  const Nil = [];
+  const Cons = x => xs => [x, ...xs];
+  const match = ({ Nil, Cons }) => xs =>
+    xs.length === 0 ? Nil : Cons(xs[0])(xs.slice(1));
 
-// Identity
-export const is = x => Array.isArray(x);
+  // Identifiable
+  const is = x => Array.isArray(x);
 
-// Functor
-export const map = f => as => as.map(f);
+  // Monoid
+  const empty = [];
+  const append = xs => ys => [...xs, ...ys];
 
-// Applicative
-export const of = x => [x];
-export const lift2 = f => a1 => a2 =>
-  a1 |> foldl(b => a => a2 |> map(f(a)) |> append(b))(empty);
+  // Foldable
+  const foldr = f => z => xs => xs.reduceRight((p, c) => f(c)(p), z);
+  const foldl = f => z => xs => xs.reduce((p, c) => f(p)(c), z);
+  const foldMap = M => f => foldr(a => M.append(f(a)))(M.empty);
 
-// Monoid
-export const empty = [];
-export const append = a1 => a2 => [...a1, ...a2];
+  // Traversable
+  const sequence = A => foldr(A.lift2(Cons))(A.of(empty));
 
-// Monad
-export const chain = f => foldl(b => a => f(a) |> append(b))(empty);
+  // Functor
+  const map = f => xs => xs.map(f);
 
-// Foldable
-export const foldl = f => z => as => as.reduce((p, c) => f(p)(c), z);
+  // Monad
+  const of = x => [x];
+  const chain = f => foldMap(Arr)(f);
 
-// Traversable
-export const sequence = A =>
-  foldl(A.lift2(b => a => of(a) |> append(b)))(A.of(empty));
+  // prettier-ignore
+  const _ = {
+    // Misc
+    last, lastN, range, filter, replicate,
+    // Constructors
+    Nil, Cons, match,
+    // Identifiable
+    is,
+    // Monoid
+    empty, append,
+    // Foldable
+    foldr, foldl, foldMap,
+    // Traversable
+    sequence,
+    // Functor
+    map,
+    // Monad
+    of, chain
+  };
+
+  return _;
+})();
+
+module.exports = Arr;
