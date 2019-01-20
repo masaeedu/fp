75103a8d4253e83f2ab517acfb28644dd675c48e
Asad Saeeduddin <masaeedu@gmail.com>
2019-01-20 16:57:10 -0500

    De-Babelify instances/str

--- src/instances/str/index.js
+++ src/instances/str/index.js
@@ -1,12 +1,18 @@
-// Identity
-export const is = x => typeof x === "string";
+const Str = (() => {
+  // Identity
+  const is = x => typeof x === "string";
 
-// Algebra
-export const Nil = "";
-export const Cons = x => xs => `${x}${xs}`;
-export const match = ({ Nil, Cons }) => s =>
-  s === "" ? Nil : Cons(s[0])(s.slice(1));
+  // Algebra
+  const Nil = "";
+  const Cons = x => xs => `${x}${xs}`;
+  const match = ({ Nil, Cons }) => s =>
+    s === "" ? Nil : Cons(s[0])(s.slice(1));
 
-// Monoid
-export const empty = "";
-export const append = a => b => a + b;
+  // Monoid
+  const empty = "";
+  const append = a => b => a + b;
+
+  return { is, Nil, Cons, match, empty, append };
+})();
+
+module.exports = Str;
