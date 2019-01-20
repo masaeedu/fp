89dd12cae30fda8f731da55fb5f624d132be68ba
Asad Saeeduddin <masaeedu@gmail.com>
2019-01-20 16:58:04 -0500

    De-Babelify instances/unit

--- src/instances/unit/index.js
+++ src/instances/unit/index.js
@@ -1,2 +1,8 @@
-// Misc
-export const unit = Symbol("@masaeedu/fp/unit");
+const Unit = (() => {
+  // Misc
+  const unit = Symbol("@masaeedu/fp/unit");
+
+  return { unit };
+})();
+
+module.exports = Unit;
