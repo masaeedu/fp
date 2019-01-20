889fad54ad4d83e84d0d153b7915e15f512ec771
Asad Saeeduddin <masaeedu@gmail.com>
2019-01-20 16:51:25 -0500

    De-Babelify instances/misc

--- src/instances/misc.js
+++ src/instances/misc.js
@@ -1,6 +1,8 @@
-export const Applicanoid = A => M => {
+const Applicanoid = A => M => {
   const empty = A.of(M.empty);
   const append = A.lift2(M.append);
 
   return { empty, append };
 };
+
+module.exports = { Applicanoid };
