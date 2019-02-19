f78d9c810b882c0c1773933d68bfd36be74fdd02
Asad Saeeduddin <masaeedu@gmail.com>
2019-02-19 03:40:10 -0500

    Fix missed argument in type parameters

--- src/classes/category.js
+++ src/classes/category.js
@@ -14,23 +14,23 @@ const methods = C => {
   // :: type Compose p p1 p2 where
   // ::      Compose p (p b c) (p a b) = p a c
 
-  // :: p1 -> p2 -> Compose p1 p2
+  // :: p1 -> p2 -> Compose p p1 p2
   _["<:"] = compose;
 
   // :: type Flip t a b = t b a
 
-  // :: p1 -> p2 -> Flip Compose p1 p2
+  // :: p1 -> p2 -> Flip Compose p p1 p2
   _[":>"] = Fn.flip(compose);
 
   // :: type Foldl r z xs where
   // ::      Foldl _ z '[]         = z
   // ::      Foldl r z '[x, ...xs] = r (Foldl r z xs) x
 
-  // :: type LPipe p xs = Foldl (Compose) (p a a) xs
+  // :: type LPipe p xs = Foldl (Compose p) (p a a) xs
   // :: xs -> LPipe p xs
   _[":[...]>"] = Arr.foldl(_[":>"])(id);
 
-  // :: type RPipe p xs = Foldl (Flip Compose) (p a a) xs
+  // :: type RPipe p xs = Foldl (Flip Compose p) (p a a) xs
   // :: xs -> RPipe p xs
   _["<[...]:"] = Arr.foldl(_["<:"])(id);
 
