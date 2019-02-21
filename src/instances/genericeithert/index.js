04bce641be9a1deede88c6bcc06b91e9cecd92c9
Asad Saeeduddin <masaeedu@gmail.com>
2019-02-21 07:41:41 -0500

    Add type annotations for genericeithert

--- src/instances/genericeithert/index.js
+++ src/instances/genericeithert/index.js
@@ -14,14 +14,19 @@ const GenericEitherT = E => M => {
   const { Left, Right, match } = E;
 
   // Monad
+  // :: x -> GenericEitherT g e m x
   const of = x => M.of(Right(x));
+
+  // :: (a -> GenericEitherT g e m b) -> GenericEitherT g e m a -> GenericEitherT g e m b
   const chain = f => M.chain(match({ Left: l => M.of(Left(l)), Right: f }));
 
   // Traversable
+  // :: Applicative f -> (a -> f b) -> GenericEitherT g e m a -> f (GenericEitherT g e m b)
   const traverse = A => f =>
     match({ Left: x => A.of(Left(x)), Right: x => A.map(Right)(f(x)) });
 
   // Bifunctor
+  // :: (l -> l') -> (r -> r') -> GenericEitherT g l m r -> GenericEitherT g l' m r'
   const bimap = l => r =>
     match({ Left: x => Left(l(x)), Right: x => Right(r(x)) });
 
